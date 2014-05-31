2be500766b3be2c3c09ba395c0fdb2b2e8666775
garymabin <garymabin@gmail.com>
2014-05-31 19:25:38 +0800

    remove useless binary files

--- proj.android/obj/local/armeabi-v7a/objs/crypto_static/sha/sha_dgst.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi-v7a/objs/crypto_static/sha/sha_dgst.o: \
- jni/../../Classes/openssl/crypto/sha/sha_dgst.c \
- jni/../../Classes/openssl/crypto/../include/openssl/opensslconf.h
-
-jni/../../Classes/openssl/crypto/../include/openssl/opensslconf.h:
